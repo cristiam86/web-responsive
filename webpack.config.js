c0b55acb3ed3d9ac94cbc7a705f80ddefdb6dedc
andeeplus <andrea.zanenghi@gmail.com>
2018-11-17 13:14:01 +0100

    uploading webpack

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: ['./index.js'],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname + "/dist"),
+        publicPath: ''
+    },
+    module:{
+      rules:[
+        {
+          test:/\.scss$/,
+          use:[
+            { loader: MiniCssExtractPlugin.loader, 
+              options: {
+                publicPath: '../'
+              } 
+            },
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        }
+      ]
+    },
+    devServer: {
+      contentBase: './dist',
+      port: 3000,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./index.html"
+      }),
+      new HtmlWebpackPlugin({
+        filename: "contacto.html",
+        template: "./contacto.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
+}
